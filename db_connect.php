d8358fcab4e6d94c77b3e177b613be87b7e2d467
Kenneth Doerflein <ktd500@verizon.net>
2025-05-18 01:38:37 -0400

    Add placeholder db password

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 //attempts to connect to database
 try {
   $dsn = 'mysql:host=localhost; dbname=sportinggoodsemporiumdatabase';
-  $db = new PDO($dsn, "sportinggoodsemporium", "d5pqwfgzbvoi465jDKJ2Bki38h6jg4k");
+  $db = new PDO($dsn, "sportinggoodsemporium", "PASSWORD_PLACEHOLDER");
 
   // set the PDO error mode to exception
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
