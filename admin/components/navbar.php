74d66e7851e0ff659121f22cd98396332712d578
Kenneth Doerflein <ktd500@verizon.net>
2025-05-18 22:13:59 -0400

    update navbars

--- admin/components/navbar.php
+++ admin/components/navbar.php
@@ -1,19 +1,34 @@
 <div class="alert alert-info text-center mb-0" role="alert" style="border-radius:0;">
   <strong>Educational Use Only:</strong> This site is for educational purposes. All content, data, and entities are entirely fictitious.
 </div>
+
 <?php
 // Only show full navbar if admin is logged in
 if (isset($_SESSION['logged_in']) && $_SESSION['userType'] == 'admin') : ?>
-  <nav class="navbar navbar-dark bg-dark mb-5">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
     <div class="container-fluid">
       <a class="navbar-brand" href="./adminHomepage.php">Home</a>
-      <a class="navbar-brand" href="./adminProducts.php">Products</a>
-      <form class="d-flex mx-auto" role="search" method="get" action="./adminProducts.php">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="searchQuery">
-        <button class="btn btn-outline-success me-3" type="submit">Search</button>
-      </form>
-      <a class="navbar-brand" href="./adminAccount.php">Account</a>
-      <a class="navbar-brand" href="../scripts/logout.php">Logout</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminNavbar" aria-controls="adminNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="adminNavbar">
+        <form class="d-flex mx-lg-auto my-3 my-lg-0" role="search" method="get" action="./adminProducts.php">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="searchQuery">
+          <button class="btn btn-outline-success me-3" type="submit">Search</button>
+        </form>
+
+        <ul class="navbar-nav ms-lg-auto text-center">
+          <li class="nav-item">
+            <a class="navbar-brand" href="./adminProducts.php">Products</a>
+          <li class="nav-item">
+            <a class="navbar-brand" href="./adminAccount.php">Account</a>
+          </li>
+          <li class="nav-item">
+            <a class="navbar-brand" href="../scripts/logout.php">Logout</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 <?php else : ?>
--- components/navbar.php
+++ components/navbar.php
@@ -1,17 +1,29 @@
 <div class="alert alert-info text-center mb-0" role="alert" style="border-radius:0;">
   <strong>Educational Use Only:</strong> This site is for educational purposes. All content, data, and entities are entirely fictitious.
 </div>
+
 <?php
 // Only show full navbar if user is logged in
 if (isset($_SESSION['logged_in'])) : ?>
-  <nav class="navbar navbar-expand navbar-dark bg-dark mb-5">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
     <div class="container-fluid">
+      <!-- Home link styled as brand -->
       <a class="navbar-brand" href="./homepage.php">Home</a>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav">
+
+      <!-- Toggler for mobile -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#userNavbar" aria-controls="userNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Collapsible content -->
+      <div class="collapse navbar-collapse" id="userNavbar">
+        <!-- Categories dropdown -->
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item dropdown">
-            <a class="navbar-brand dropdown-toggle" href="#" data-bs-toggle="dropdown">Categories</a>
-            <ul class="dropdown-menu">
+            <a class="navbar-brand nav-link dropdown-toggle" href="#" id="categoryDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Categories
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="categoryDropdown">
               <li><a class="dropdown-item" href="./homepage.php?category_id=All">All Products</a></li>
               <li><a class="dropdown-item" href="./homepage.php?category_id=Tops">Tops</a></li>
               <li><a class="dropdown-item" href="./homepage.php?category_id=Bottoms">Bottoms</a></li>
@@ -22,14 +34,20 @@ if (isset($_SESSION['logged_in'])) : ?>
             </ul>
           </li>
         </ul>
+
+        <!-- Search form -->
+        <form class="d-flex my-2 my-lg-0 me-lg-3" role="search" method="get" action="./homepage.php">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="searchQuery">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+
+        <!-- Right-side links styled as navbar-brand for consistency -->
+        <div class="d-flex flex-column flex-lg-row">
+          <a class="navbar-brand" href="./cart.php">Cart</a>
+          <a class="navbar-brand" href="./account.php">Account</a>
+          <a class="navbar-brand" href="./scripts/logout.php">Logout</a>
+        </div>
       </div>
-      <form class="d-flex" role="search" method="get" action="./homepage.php">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="searchQuery">
-        <button class="btn btn-outline-success me-3" type="submit">Search</button>
-      </form>
-      <a class="navbar-brand" href="./cart.php">Cart</a>
-      <a class="navbar-brand" href="./account.php">Account</a>
-      <a class="navbar-brand" href="./scripts/logout.php">Logout</a>
     </div>
   </nav>
 <?php else : ?>
